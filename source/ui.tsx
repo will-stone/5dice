ffa51e03588e3b8dffc4a974aa13581a8eaf344c
Will Stone <654103+will-stone@users.noreply.github.com>
2021-02-17 09:20:18 +0000

    LabelBox component

--- source/ui.tsx
+++ source/ui.tsx
@@ -1,11 +1,19 @@
 /* eslint-disable react/jsx-newline */
 
-import { Box, Spacer, Text, useApp, useInput } from 'ink'
+import {
+  Box,
+  DOMElement,
+  measureElement,
+  Spacer,
+  Text,
+  useApp,
+  useInput,
+} from 'ink'
 import Divider from 'ink-divider'
 import Gradient from 'ink-gradient'
 import Link from 'ink-link'
 import _ from 'lodash'
-import React, { useReducer } from 'react'
+import React, { useEffect, useReducer, useRef, useState } from 'react'
 
 import { ScoreIds } from './model'
 import {
@@ -36,6 +44,47 @@ const lowerBoard: { [key: string]: ScoreIds } = {
   T: 'largeStraight',
   Y: 'chance',
   U: 'fiveDice',
+}
+
+const LabelBox: React.FC<{ label: string }> = ({ children, label }) => {
+  const reference = useRef<DOMElement>(null)
+  const [{ width }, setMeasurements] = useState({ width: 0, height: 0 })
+
+  useEffect(() => {
+    setMeasurements(measureElement(reference.current as DOMElement))
+  }, [])
+
+  if (width > 0) {
+    return (
+      <Box ref={reference} flexDirection="column">
+        <Box>
+          <Text dimColor>
+            ╭ {label} {'─'.repeat(width - 4 - label.length)}╮
+          </Text>
+        </Box>
+        <Box flexDirection="column">
+          {React.Children.map(children, (child) => {
+            if (React.isValidElement(child)) {
+              return (
+                <Box flexGrow={1}>
+                  <Text dimColor>│ </Text>
+                  <Box flexGrow={1}>{React.cloneElement(child)}</Box>
+                  <Text dimColor> │</Text>
+                </Box>
+              )
+            }
+
+            return child
+          })}
+        </Box>
+        <Box>
+          <Text dimColor>╰{'─'.repeat(width - 2)}╯</Text>
+        </Box>
+      </Box>
+    )
+  }
+
+  return <Box ref={reference} />
 }
 
 const App: React.FC = () => {
@@ -91,119 +140,72 @@ const App: React.FC = () => {
       </Box>
 
       <Box>
-        <Box flexDirection="column">
-          <Box flexDirection="column">
-            <Box>
-              <Text dimColor>╭ Turn ─────╮</Text>
-            </Box>
-            <Box>
-              <Text dimColor>│ </Text>
-              <Text dimColor={turn === 0}>{turn}</Text>
-              <Spacer />
-              <Text dimColor>│</Text>
-            </Box>
-            <Box>
-              <Text dimColor>╰───────────╯</Text>
-            </Box>
-          </Box>
+        <Box flexDirection="column" width={13}>
+          <LabelBox label="Turn">
+            <Text dimColor={turn === 0}>{turn}</Text>
+          </LabelBox>
 
-          <Box flexDirection="column">
-            <Box>
-              <Text dimColor>╭ Dice ─────╮</Text>
-            </Box>
-            <Box>
-              <Text dimColor>│ </Text>
-              <Text dimColor>A S D F G</Text>
-              <Text dimColor> │</Text>
-            </Box>
-            <Box>
-              <Text dimColor>│ </Text>
-              {dice.map(([id, { value, held }], index) => (
-                <React.Fragment key={id}>
-                  {index !== 0 && <Text> </Text>}
-
-                  <Text dimColor={value === 0} inverse={held}>
-                    {value || '-'}
-                  </Text>
-                </React.Fragment>
+          <LabelBox label="Dice">
+            <Text dimColor>A S D F G</Text>
+            <Box flexGrow={1} justifyContent="space-between">
+              {dice.map(([id, { value, held }]) => (
+                <Text key={id} dimColor={value === 0} inverse={held}>
+                  {value || '-'}
+                </Text>
               ))}
-              <Text dimColor> │</Text>
             </Box>
-            <Box>
-              <Text dimColor>╰───────────╯</Text>
-            </Box>
-          </Box>
+          </LabelBox>
         </Box>
 
         <Box flexDirection="column" width={16}>
-          <Box>
-            <Text dimColor>╭ Upper Board ─╮</Text>
-          </Box>
-          {Object.entries(upperBoard).map(([hotkey, id]) => (
-            <Box key={id}>
-              <Text dimColor>│ </Text>
-              <Text dimColor={!_.isString(scores[id])}>{hotkey} </Text>
-              <Text dimColor={!_.isString(scores[id])}>{_.startCase(id)}</Text>
-              <Spacer />
-              <Box justifyContent="flex-end" minWidth={2}>
-                <Text dimColor={_.isNumber(scores[id])}>{scores[id]}</Text>
+          <LabelBox label="Upper Board">
+            {Object.entries(upperBoard).map(([hotkey, id]) => (
+              <Box key={id} flexGrow={1}>
+                <Text dimColor={!_.isString(scores[id])}>{hotkey} </Text>
+                <Text dimColor={!_.isString(scores[id])}>
+                  {_.startCase(id)}
+                </Text>
+                <Spacer />
+                <Box justifyContent="flex-end" minWidth={2}>
+                  <Text dimColor={_.isNumber(scores[id])}>{scores[id]}</Text>
+                </Box>
               </Box>
-              <Text dimColor> │</Text>
+            ))}
+            <Text dimColor>────────────</Text>
+            <Box flexGrow={1}>
+              <Text dimColor={upperBoardSum === 0}>Sum</Text>
+              <Spacer />
+              <Text dimColor={upperBoardSum === 0}>{upperBoardSum}</Text>
             </Box>
-          ))}
-          <Box>
-            <Text dimColor>│ ──────────── │</Text>
-          </Box>
-          <Box>
-            <Text dimColor>│ </Text>
-            <Text dimColor={upperBoardSum === 0}>Sum</Text>
-            <Spacer />
-            <Text dimColor={upperBoardSum === 0}>{upperBoardSum}</Text>
-            <Text dimColor> │</Text>
-          </Box>
-          <Box>
-            <Text dimColor>│ </Text>
-            <Text dimColor={upperBoardBonus < 35}>Bonus</Text>
-            <Spacer />
-            <Text dimColor={upperBoardBonus < 35}>{upperBoardBonus}</Text>
-            <Text dimColor> │</Text>
-          </Box>
-          <Box>
-            <Text dimColor>╰──────────────╯</Text>
-          </Box>
+            <Box flexGrow={1}>
+              <Text dimColor={upperBoardBonus < 35}>Bonus</Text>
+              <Spacer />
+              <Text dimColor={upperBoardBonus < 35}>{upperBoardBonus}</Text>
+            </Box>
+          </LabelBox>
         </Box>
 
         <Box flexDirection="column" width={25}>
-          <Box>
-            <Text dimColor>╭ Lower Board ──────────╮</Text>
-          </Box>
-          {Object.entries(lowerBoard).map(([hotkey, id]) => (
-            <Box key={id}>
-              <Text dimColor>│ </Text>
-              <Text dimColor={!_.isString(scores[id])}>{hotkey} </Text>
-              <Text dimColor={!_.isString(scores[id])}>{_.startCase(id)}</Text>
-              <Spacer />
-              <Box justifyContent="flex-end" minWidth={2}>
-                <Text dimColor={_.isNumber(scores[id])}>{scores[id]}</Text>
+          <LabelBox label="Lower Board">
+            {Object.entries(lowerBoard).map(([hotkey, id]) => (
+              <Box key={id} flexGrow={1}>
+                <Text dimColor={!_.isString(scores[id])}>{hotkey} </Text>
+                <Text dimColor={!_.isString(scores[id])}>
+                  {_.startCase(id)}
+                </Text>
+                <Spacer />
+                <Box justifyContent="flex-end" minWidth={2}>
+                  <Text dimColor={_.isNumber(scores[id])}>{scores[id]}</Text>
+                </Box>
               </Box>
-              <Text dimColor> │</Text>
-            </Box>
-          ))}
-          <Box>
-            <Text dimColor>│ </Text>
+            ))}
             <Text dimColor>═════════════════════</Text>
-            <Text dimColor> │</Text>
-          </Box>
-          <Box>
-            <Text dimColor>│ </Text>
-            <Text dimColor={total === 0}>Total</Text>
-            <Spacer />
-            <Text dimColor={total === 0}>{total}</Text>
-            <Text dimColor> │</Text>
-          </Box>
-          <Box>
-            <Text dimColor>╰───────────────────────╯</Text>
-          </Box>
+            <Box flexGrow={1}>
+              <Text dimColor={total === 0}>Total</Text>
+              <Spacer />
+              <Text dimColor={total === 0}>{total}</Text>
+            </Box>
+          </LabelBox>
         </Box>
       </Box>
 
