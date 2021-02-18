ff54878f6518d2ed5f4e4143e6e368beb51b0411
Will Stone <654103+will-stone@users.noreply.github.com>
2021-02-18 07:58:22 +0000

    Start storing scores

--- source/store.ts
+++ source/store.ts
@@ -1,5 +1,8 @@
 import { createAction, createReducer } from '@reduxjs/toolkit'
 import _ from 'lodash'
+import os from 'os'
+import path from 'path'
+import writeJsonFile from 'write-json-file'
 
 import { calculatePotentialScores } from './calculate-potential-scores'
 import type { DieId, Score, ScoreIds, Scores } from './model'
@@ -48,6 +51,45 @@ export const initialState: State = {
 
 const selectIsGameOver = (state: State) => {
   return Object.values(state.scores).every((s) => _.isNumber(s))
+}
+
+export const selectUpperBoardSum = (state: State): number => {
+  const upperBoard = [
+    state.scores.ones,
+    state.scores.twos,
+    state.scores.threes,
+    state.scores.fours,
+    state.scores.fives,
+    state.scores.sixes,
+  ]
+  const numbers = upperBoard.filter(_.isNumber)
+  return _.sum(numbers)
+}
+
+export const selectUpperBoardBonus = (state: State): number => {
+  return selectUpperBoardSum(state) >= 63 ? 35 : 0
+}
+
+const selectLowerBoardSum = (state: State): number => {
+  const lowerBoard = [
+    state.scores.threeOfAKind,
+    state.scores.fourOfAKind,
+    state.scores.fullHouse,
+    state.scores.smallStraight,
+    state.scores.largeStraight,
+    state.scores.chance,
+    state.scores.fiveDice,
+  ]
+  const numbers = lowerBoard.filter(_.isNumber)
+  return _.sum(numbers)
+}
+
+export const selectTotal = (state: State): number => {
+  return _.sum([
+    selectUpperBoardSum(state),
+    selectUpperBoardBonus(state),
+    selectLowerBoardSum(state),
+  ])
 }
 
 export const reducer = createReducer(initialState, (builder) => {
@@ -108,47 +150,14 @@ export const reducer = createReducer(initialState, (builder) => {
         state.dice = initialState.dice
         state.turn = 0
       }
+
+      if (selectIsGameOver(state)) {
+        writeJsonFile(path.join(os.homedir(), '5dice.json'), {
+          topScores: [{ score: selectTotal(state) }],
+        })
+      }
     })
     .addCase(restartGame, () => {
       return initialState
     })
 })
-
-export const selectUpperBoardSum = (state: State): number => {
-  const upperBoard = [
-    state.scores.ones,
-    state.scores.twos,
-    state.scores.threes,
-    state.scores.fours,
-    state.scores.fives,
-    state.scores.sixes,
-  ]
-  const numbers = upperBoard.filter(_.isNumber)
-  return _.sum(numbers)
-}
-
-export const selectUpperBoardBonus = (state: State): number => {
-  return selectUpperBoardSum(state) >= 63 ? 35 : 0
-}
-
-const selectLowerBoardSum = (state: State): number => {
-  const lowerBoard = [
-    state.scores.threeOfAKind,
-    state.scores.fourOfAKind,
-    state.scores.fullHouse,
-    state.scores.smallStraight,
-    state.scores.largeStraight,
-    state.scores.chance,
-    state.scores.fiveDice,
-  ]
-  const numbers = lowerBoard.filter(_.isNumber)
-  return _.sum(numbers)
-}
-
-export const selectTotal = (state: State): number => {
-  return _.sum([
-    selectUpperBoardSum(state),
-    selectUpperBoardBonus(state),
-    selectLowerBoardSum(state),
-  ])
-}
