84476745cc0d20e08f0a11df207105b01a8df968
Will Stone <654103+will-stone@users.noreply.github.com>
2021-02-22 20:43:25 +0000

    Top scores

--- source/cli.tsx
+++ source/cli.tsx
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import { render } from 'ink'
 import jsonfile from 'jsonfile'
+import { autorun, toJS } from 'mobx'
 import React from 'react'
 
 import { GameEngine } from './game-engine'
@@ -10,6 +11,12 @@ import App from './ui'
 const savedFile = jsonfile.readFileSync('5dice.json', { throws: false })
 
 const game = new GameEngine(savedFile)
+
+autorun(() => {
+  if (game.rolling === false) {
+    jsonfile.writeFileSync('5dice.json', toJS(game))
+  }
+})
 
 // eslint-disable-next-line no-console
 console.clear()
