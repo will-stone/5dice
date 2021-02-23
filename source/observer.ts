3b91d20188839840caa4fa375a94a418aaa951fb
Will Stone <654103+will-stone@users.noreply.github.com>
2021-02-23 08:24:43 +0000

    Comment

--- source/observer.ts
+++ source/observer.ts
@@ -1,5 +1,6 @@
 import { observer as distObserver } from 'mobx-react-lite/dist/observer'
-// @ts-expect-error workaround as mobx-react is expecting react-dom
+// @ts-expect-error workaround as mobx-react is expecting react-dom which isn't
+// available here, in the Ink renderer environment.
 import { observer as libraryObserver } from 'mobx-react-lite/lib/observer'
 
 type Observer = typeof distObserver
