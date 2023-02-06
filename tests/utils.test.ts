c286f4a296218d99f833f5b4ed395aa8ac2f457f
Will Stone <654103+will-stone@users.noreply.github.com>
2023-02-06 21:46:47 +0000

    test: increase code coverage

---
+++ tests/utils.test.ts
@@ -0,0 +1,33 @@
+import { biasedD6, d6, toKeys, toPairs } from '../source/utils'
+
+test('should convert to pairs', () => {
+  expect(toPairs({ a: 1, b: 2 })).toStrictEqual([
+    ['a', 1],
+    ['b', 2],
+  ])
+})
+
+test('should convert to keys', () => {
+  expect(toKeys({ a: 1, b: 2 })).toStrictEqual(['a', 'b'])
+})
+
+test('should roll a d6', () => {
+  const randomSpy = jest.spyOn(global.Math, 'random')
+  randomSpy.mockReturnValueOnce(0.123)
+  expect(d6()).toBe(1)
+  randomSpy.mockReturnValueOnce(0.422)
+  expect(d6()).toBe(3)
+  randomSpy.mockReturnValueOnce(0.999_99)
+  expect(d6()).toBe(6)
+  randomSpy.mockRestore()
+})
+
+test('should roll a biased d6', () => {
+  const randomSpy = jest.spyOn(global.Math, 'random')
+  randomSpy.mockReturnValueOnce(0.123)
+  randomSpy.mockReturnValueOnce(0.152)
+  randomSpy.mockReturnValueOnce(0.522)
+  expect(biasedD6(1)).toBe(4)
+  expect(randomSpy).toHaveBeenCalledTimes(3)
+  randomSpy.mockRestore()
+})
